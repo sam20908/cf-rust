bf86885657a67728104268090aeabf98d0715ae8
Sam Huang <samestimable2016@gmail.com>
2023-09-26 09:35:23 -0700

    Add binary search

---
+++ src/binary_search.rs
@@ -0,0 +1,27 @@
+use std::ops::*;
+
+pub fn first_true<F: Fn(i64) -> bool>(mut lo: i64, mut hi: i64, f: F) -> i64 {
+    hi += 1;
+    while lo < hi {
+        let mid = lo + (hi - lo) >> 1;
+        if f(mid) {
+            hi = mid;
+        } else {
+            lo = mid + 1;
+        }
+    }
+    lo
+}
+
+pub fn last_true<F: Fn(i64) -> bool>(mut lo: i64, mut hi: i64, f: F) -> i64 {
+    lo -= 1;
+    while lo < hi {
+        let mid = lo + (hi - lo + 1) >> 1;
+        if f(mid) {
+            lo = mid;
+        } else {
+            hi = mid - 1;
+        }
+    }
+    lo
+}
--- src/lib.rs
+++ src/lib.rs
@@ -1,9 +1,11 @@
+mod binary_search;
 mod dsu;
 mod lca;
 mod modnum;
 mod sieve;
 
-pub use dsu::DSU;
-pub use lca::LCA;
-pub use modnum::Modnum;
-pub use sieve::Sieve;
+pub use binary_search::*;
+pub use dsu::*;
+pub use lca::*;
+pub use modnum::*;
+pub use sieve::*;
