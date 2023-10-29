953f812328a33f6d3bd2d0d4f787dc67e6737933
Sam Huang <samestimable2016@gmail.com>
2023-10-29 09:06:21 -0700

    Skip empty lines

--- src/main.rs
+++ src/main.rs
@@ -13,13 +13,21 @@ struct Pair<U, V> {
     second: V,
 }
 
-macro_rules! readline {
+macro_rules! read_line {
     ($($x:ty),+) => {{
         let mut buf = String::new();
-        let _ = io::stdin().read_line(&mut buf);
+        while buf.is_empty() {
+            let _ = io::stdin().read_line(&mut buf);
+        }
         let mut iter = buf.split(char::is_whitespace);
         ($(iter.next().unwrap().parse::<$x>().unwrap()),*)
     }}
+}
+fn read_vec<T: std::str::FromStr>() -> Vec<T> {
+    read_line!(String)
+        .split(' ')
+        .flat_map(|s| s.parse())
+        .collect()
 }
 
 fn solve(out: &mut BufWriter<StdoutLock<'_>>) {}
@@ -27,7 +35,7 @@ fn solve(out: &mut BufWriter<StdoutLock<'_>>) {}
 fn main() {
     let stdout = io::stdout();
     let mut out = BufWriter::new(stdout.lock());
-    let t = readline!(i32);
+    let t = read_line!(i32);
     for _ in 0..t {
         solve(&mut out);
     }
